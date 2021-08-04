133b38fa57db8552e3745f7202119941e177f28c
Ranju <ranjugc69@gmail.com>
2021-08-04 10:46:49 +0545

    vector matrix arithmetic added to replace glm

---
+++ include/VecMat/arithmetic.hpp
@@ -0,0 +1,163 @@
+#include "matrix.hpp"
+#include "vector.hpp"
+
+namespace VecMat {
+	//mat3 constructor
+	mat3::mat3(mat4 mat4){
+		for (int i = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++){
+				mat[i][j] = mat4.mat[i][j];
+			}
+		}
+		
+	}
+
+
+
+	//matrix multiplication
+	mat4 operator *(mat4 a, mat4 b) {
+		mat4 result;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				for (int k = 0; k < 4; k++) {
+					result.mat[i][j] += a.mat[i][k] * b.mat[k][j];
+				}
+			}
+		}
+		return result;
+	}
+
+
+	mat4 translate(const mat4& mat, float tx, float ty, float tz) {
+		mat4 result = mat;
+		result.mat[0][3] = tx;
+		result.mat[1][3] = ty;
+		result.mat[1][3] = tz;
+		return result;
+	}
+
+
+
+	mat4 translate(const mat4& mat, vec3 vec) {
+		mat4 result = mat;
+		result.mat[0][3] = vec.x;
+		result.mat[1][3] = vec.y;
+		result.mat[2][3] = vec.z;
+
+		return result;
+	}
+
+	mat4 rotateX(const mat4& matrix, float angle) {
+		mat4 result(1.0f);
+
+		result.mat[1][1] = cos(angle);
+		result.mat[1][2] = -sin(angle);
+		result.mat[2][1] = sin(angle);
+		result.mat[2][2] = cos(angle);
+
+		return result * matrix;
+	}
+
+
+	mat4 rotateY(const mat4& matrix, float angle) {
+		mat4 result(1.0f);
+
+		result.mat[0][0] = cos(angle);
+		result.mat[0][2] = sin(angle);
+		result.mat[2][0] = -sin(angle);
+		result.mat[2][2] = result.mat[0][0];
+
+
+		return result * matrix;
+	}
+
+
+	mat4 rotateZ(const mat4& matrix, float angle) {
+		mat4 result(1.0f);
+
+		result.mat[0][0] = cos(angle);
+		result.mat[0][1] = -sin(angle);
+		result.mat[1][0] = sin(angle);
+		result.mat[1][1] = cos(angle);
+
+		return result * matrix;
+	}
+
+
+	mat4 rotate(const mat4& matrix, const vec3& vec, const vec3& point, const float angle) {
+		mat4 result(1.0f);
+		vec3 axis = vec.unitVector();
+
+		result = translate(result, -point);
+		result = rotate(matrix, vec, angle);
+		result = translate(result, point);
+		return result * matrix;
+	}
+
+	mat4 rotate(const mat4& matrix, const vec3& vec, const float angle) {
+		mat4 result(1.0f);
+		vec3 axis = vec.unitVector();
+
+		const float sinVal = sin(angle);
+		const float cosVal = cos(angle);
+
+		const float x = axis.x;
+		const float y = axis.y;
+		const float z = axis.z;
+
+		result.mat[0][0] = cosVal + x * x * (1 - cosVal);
+		result.mat[0][1] = x * y * (1 - cosVal) - z * sinVal;
+		result.mat[0][2] = x * z * (1 - cosVal) + y * sinVal;
+
+		result.mat[1][0] = x * y * (1 - cosVal) + z * sinVal;
+		result.mat[1][1] = cosVal + y * y * (1 - cosVal);
+		result.mat[1][2] = z * y * (1 - cosVal) - x * sinVal;
+
+		result.mat[2][0] = x * z * (1 - cosVal) - y * sinVal;
+		result.mat[2][1] = z * y * (1 - cosVal) + x * sinVal;
+		result.mat[2][2] = cosVal + z * z * (1 - cosVal);
+
+		return result * matrix;
+	}
+
+	mat4 lookAt(const vec3& eye, const vec3& center, const vec3& up) {
+		mat4 result(1.0f);
+
+		auto f = (-center + eye).unitVector();
+		auto s = cross(up, f).unitVector();
+		auto u = cross(f, s).unitVector();
+
+		result.mat[0][0] = s.x;
+		result.mat[1][0] = s.y;
+		result.mat[2][0] = s.z;
+
+
+		result.mat[0][1] = u.x;
+		result.mat[1][1] = u.y;
+		result.mat[2][1] = u.z;
+
+
+		result.mat[0][2] = f.x;
+		result.mat[1][2] = f.y;
+		result.mat[2][2] = f.z;
+
+
+		result.mat[3][0] = -s.dot(eye);
+		result.mat[3][1] = -u.dot(eye);
+		result.mat[3][2] = -f.dot(eye);
+
+		return result;
+	}
+
+
+	mat4 perspective(float fov, float aspectRatio, float near = 0.1, float far = 100.0f){
+		mat4 result;
+        float scale = 1.0 / tan(to_radians(45/2.0));
+        result[0][0] = scale/aspectRatio; // scale the x coordinates
+        result[1][1] = scale; // scale the y coordinates
+        result[2][2] = -(far+near) / (far - near); // remap z to [0,1] 
+        result[2][3] = -1; 
+        result[3][2] = (2*far*near)/(near - far); 
+		return result;
+	}
+}
---
+++ include/VecMat/matrix.hpp
@@ -0,0 +1,209 @@
+#pragma once
+#include <vector>
+#include "vector.hpp"
+#include <iostream>
+
+
+namespace VecMat {
+
+	struct mat4;
+
+	struct mat3{
+		float mat[3][3];
+
+		mat3() {
+			
+		}
+
+		mat3(float x){
+			for (int i = 0; i < 3; i++) {
+				mat[i][i] = x;
+			}
+		}
+
+		mat3(mat4 mat4);
+
+		float& operator [](const std::pair<int, int>& index) {
+			return mat[index.first][index.second];
+		}
+ 
+		mat3 operator +(mat3 m) {
+			mat3 result;
+
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					result.mat[i][j] = mat[i][j] + m.mat[i][j];
+				}
+			}
+			return result.transpose();
+		}
+
+
+
+		mat3 operator -(mat3 m) {
+			mat3 result;
+
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					result.mat[i][j] = mat[i][j] - m.mat[i][j];
+				}
+			}
+			return result.transpose();
+		}
+
+		bool operator ==(mat3 m) {
+			// bool flag = true;
+			// for (int i = 0; i < 3; i++) {
+			// 	flag = this->mat[i] == m[i];
+			// 	if (!flag)
+			// 		return flag;
+			// }
+			// return flag;
+			return false;
+		}
+
+		float* value_ptr() {
+			return &mat[0][0];
+		}
+
+
+		mat3 transpose(){
+			mat3 result;
+
+			for(int i = 0; i < 3; i++){
+				for(int j = 0; j < 3; j++){
+					result.mat[i][j] = mat[j][i];
+				}
+			}
+			return result.transpose();
+		}
+	};
+
+	 inline std::ostream& operator<<(std::ostream& os, mat3 matrix){
+    	std::cout << "\nOutput in Row major order -> \n";
+    	for (int i = 0; i < 3; ++i)
+    	{
+    	  for (int j = 0; j < 3; ++j)
+    	  {
+	os 	<< '[' << i << ']' << '[' << j << ']' <<" -> " << matrix.mat[j][i] << "   "; 
+    	  }
+    	  os << '\n';
+    	}
+    	return os << std::endl;
+	};
+
+
+	struct mat4 {
+		float mat[4][4] =  {0.0f};
+
+		mat4() {
+			
+		}
+		
+
+		mat4(mat3 mat3){
+			for (int i = 0; i < 3; i++){
+				for(int j = 0; j < 3; j++){
+					mat[i][j] = mat3.mat[i][j];
+				}
+			}
+			mat[3][3] = 1.0f;
+		}
+
+		mat4(float x){
+			for (int i = 0; i < 4; i++) {
+				mat[i][i] = x;
+			}
+		}
+
+		float* operator [](const int index){
+			return mat[index];
+		}
+
+		float& operator [](const std::pair<int, int>& index) {
+			return mat[index.first][index.second];
+		}
+ 
+		mat4 operator +(mat4 m) {
+			mat4 result;
+
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					result.mat[i][j] = mat[i][j] + m.mat[i][j];
+				}
+			}
+			return result.transpose();
+		}
+
+
+
+		mat4 operator -(mat4 m) {
+			mat4 result;
+
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					result.mat[i][j] = mat[i][j] - m.mat[i][j];
+				}
+			}
+			return result.transpose();
+		}
+
+		bool operator ==(mat4 m) {
+			// bool flag = true;
+			// for (int i = 0; i < 4; i++) {
+			// 	flag = this->mat[i] == m[i];
+			// 	if (!flag)
+			// 		return flag;
+			// }
+			// return flag;
+			return false;
+		}
+
+		float* value_ptr() {
+			return &mat[0][0];
+		}
+
+
+		mat4 transpose(){
+			mat4 result;
+
+			for(int i = 0; i < 4; i++){
+				for(int j = 0; j < 4; j++){
+					result.mat[i][j] = mat[j][i];
+				}
+			}
+			return result.transpose();
+		}
+	};
+
+	 inline std::ostream& operator<<(std::ostream& os, mat4 matrix){
+    	std::cout << "\nOutput in Row major order -> \n";
+    	for (int i = 0; i < 4; ++i)
+    	{
+    	  for (int j = 0; j < 4; ++j)
+    	  {
+	os 	<< '[' << i << ']' << '[' << j << ']' <<" -> " << matrix.mat[j][i] << "   "; 
+    	  }
+    	  os << '\n';
+    	}
+    	return os << std::endl;
+  	}
+
+	mat4 operator *(mat4 a, mat4 b);
+
+	mat4 translate(const mat4& mat, float tx, float ty, float tz);
+
+	mat4 translate(const mat4& mat, vec3 vec);
+
+	mat4 scale(const mat4& mat, vec3 vec);
+
+	mat4 rotateX(const mat4& matrix, float angle);
+
+	mat4 rotateY(const mat4& matrix, float angle);
+
+	mat4 rotateZ(const mat4& matrix, float angle);
+
+	mat4 rotate(const mat4& matrix, const vec3& vec, const float angle);
+
+	mat4 lookAt(const vec3& cameraPos, const vec3& target, const vec3& up);
+}
